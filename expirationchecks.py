b701655c0be64207991aee15a0c7a8d949e91ee2
RTa-technology <scpfoundationidcardmaker@gmail.com>
2022-06-09 16:05:45 +0900

    add: Expiration Checks

---
+++ expirationchecks.py
@@ -0,0 +1,17 @@
+import frontmatter
+from datetime import datetime as dt
+from pytz import timezone
+import os
+from pathlib import Path
+
+reserves = Path("src/reserve").glob('**/*.md')
+today = dt.now().astimezone(timezone('Asia/Tokyo')).date()
+for filename in reserves:
+    with open(filename, 'r', encoding='utf-8') as f:
+        data = frontmatter.load(f)
+    # data['endtime']が今日の日付よりも過去の日付ならファイルを削除する
+    endtime = dt.strptime(data['endtime'], '%Y-%m-%d').date()
+    print(filename,"\t\t" ,endtime)
+    if endtime <= today:
+        print('{} is expired'.format(filename))
+        os.remove(filename)
